dce05d09b4cd51788d7d42506c2719396893c74b
sapnull <sapnull@local.dev>
2025-03-02 03:04:49 -0500

    Adding management lock functionality to main.tf

--- main.tf
+++ main.tf
@@ -13,10 +13,10 @@ resource "azurerm_resource_group" "rg" {
   })
 }
 
-# resource "azurerm_management_lock" "rg_lock" {
-#   count      = var.rg_lock == "" ? 0 : 1
-#   name       = "${var.rg_name}-mgmt-lock"
-#   scope      = azurerm_resource_group.rg.id
-#   lock_level = var.rg_lock
-#   notes      = "Resource Group: '${var.rg_name}' is locked with '${var.rg_lock}' type mgmt lock."
-# }
+resource "azurerm_management_lock" "rg_lock" {
+  count      = var.rg_lock == "" ? 0 : 1
+  name       = "${var.name}-mgmt-lock"
+  scope      = azurerm_resource_group.rg.id
+  lock_level = var.rg_lock
+  notes      = "Resource Group: '${var.name}' is locked with '${var.rg_lock}' type mgmt lock."
+}
